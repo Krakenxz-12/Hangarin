1d9ffe316e0197374a334ac3284824bff4037afe
Krakenxz-12 <202480133@psu.palawan.edu.ph>
2026-09-24 23:38:18 +0800

    Faker and Bootstrap added and initiated

--- hangarinsite/hangarin/admin.py
+++ hangarinsite/hangarin/admin.py
@@ -1,3 +1,69 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Priority, Category, Task, Note, SubTask)
+
+
+@admin.register(Priority)
+class PriorityAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at")
+    search_fields = ("name",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at")
+    search_fields = ("name",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "category",
+        "priority",
+        "status",
+        "deadline",
+    )
+
+    search_fields = (
+        "title",
+        "description",
+    )
+
+    list_filter = (
+        "status",
+        "category",
+        "priority",
+    )
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = (
+        "task",
+        "content",
+        "created_at",
+    )
+
+    search_fields = (
+        "content",
+        "task__title",
+    )
+
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "parent_task",
+        "status",
+    )
+
+    search_fields = (
+        "title",
+        "parent_task__title",
+    )
+
+    list_filter = (
+        "status",
+    )
--- hangarinsite/hangarinsite/settings.py
+++ hangarinsite/hangarinsite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hangarin',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
